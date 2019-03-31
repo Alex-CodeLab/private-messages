0e92630c19b7a24e2861cf5e9440f204144cbe63
alex <alex.sd3@gmail.com>
2019-03-31 13:34:59 +0200

    add template

--- models.py
+++ models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import signals
+from django.utils import timezone
+import uuid
 
-# Create your models here.
+def _uuid():
+    return str(uuid.uuid4()).replace('-','')
+
+class Conversation(models.Model):
+    startdate   = models.DateTimeField(auto_now_add=True)
+    users       = models.ManyToManyField(User)
+    hash        = models.CharField(max_length=32,default=_uuid,unique=True)
+
+
+class PrivateMessage(models.Model):
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
+    text    = models.TextField('text')
+    persona = models.ForeignKey(User, related_name='persona', on_delete=models.CASCADE)
+    personb = models.ForeignKey(User, related_name='personb', on_delete=models.CASCADE)
+    create_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text[0:40]
--- views.py
+++ views.py
@@ -1,3 +1,18 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Conversation
+
+def conversations(request):
+    conversations  = Conversation.objects.filter()
+    print(conversations)
+    return render(request, 'private_messages.html',{'conversations': conversations})
+
+
+def conversation(request):
+    #list
+    try:
+        conversation = Conversation.objects.get(hash= conversation_id)
+    except ObjectDoesNotExist:
+        pass
+    return render(request, 'private_messages.html',{})

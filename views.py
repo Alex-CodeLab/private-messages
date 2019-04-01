3cc29966331df3d89e64b229bb08f3bc97246357
alex <alex.sd3@gmail.com>
2019-04-01 12:09:25 +0200

    query contacts

--- views.py
+++ views.py
@@ -1,11 +1,13 @@
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 from .models import Conversation
 
 def conversations(request):
-    conversations  = Conversation.objects.filter()
-    contacts  = Conversation.objects.filter(users=request.user).values_list('users__username').distinct()
+    conversations  = Conversation.objects.filter(users=request.user)
+    userids = conversations.values_list('users__id').distinct()
+    contacts = User.objects.filter(id__in=userids)
     return render(request, 'private_messages.html',{'contacts': contacts, 'conversations': conversations})
 
 
